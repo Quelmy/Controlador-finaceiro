16ff3c491f2b3d372af9a806d8f251667c3661c2
Quelmy <Ferreirariquelmy27@gmail.com>
2025-09-02 15:32:44 -0300

    Update index.js
    alterações de components

--- src/components/Grid/index.js
+++ src/components/Grid/index.js
@@ -3,29 +3,29 @@ import GridItem from "../GridItem";
 import * as C from "./styles";
 
 const Grid = ({ itens, setItens }) => {
-    const onDelete = (ID) => {
-        const newArray = itens.filter((transaction) => transaction.id !== ID);
-        setItens(newArray);
-        localStorage.setItem("transaction", JSON.stringify(newArray));
-    };
+  const onDelete = (ID) => {
+    const newArray = itens.filter((transaction) => transaction.id !== ID);
+    setItens(newArray);
+    localStorage.setItem("transactions", JSON.stringify(newArray));
+  };
 
-    return (
-        <C.Table>
-            <C.Thead>
-                <C.Tr>
-                    <C.Th width={40}>Descrição</C.Th>
-                    <C.Th width={40}>Valor</C.Th>
-                    <C.Th width={10} alignCenter>Tipo</C.Th>
-                    <C.Th width={10}></C.Th>
-                </C.Tr>
-            </C.Thead>
-            <C.Tbody>
-                {itens?.map((item) => (
-                    <GridItem key={item.id} item={item} onDelete={onDelete} />
-                ))}
-            </C.Tbody>
-        </C.Table>
-    );
+  return (
+    <C.Table>
+      <C.Thead>
+        <C.Tr>
+          <C.Th width={40}>Descrição</C.Th>
+          <C.Th width={30}>Valor</C.Th>
+          <C.Th width={15} alignCenter>Tipo</C.Th>
+          <C.Th width={15}></C.Th>
+        </C.Tr>
+      </C.Thead>
+      <C.Tbody>
+        {itens?.map((item) => (
+          <GridItem key={item.id} item={item} onDelete={onDelete} />
+        ))}
+      </C.Tbody>
+    </C.Table>
+  );
 };
 
 export default Grid;
